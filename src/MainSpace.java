619dcf71365aadce848adf52435d169c8f1f3b71
Lorenzo Alberti <alberti.lorenzo.110903>
2022-04-01 16:13:50 +0200

    alv utf8 and density print

--- src/MainSpace.java
+++ src/MainSpace.java
@@ -10,8 +10,9 @@ public class MainSpace
 		List<Body.Moons> lune = rc.getBody("mars",true).getMoons();
 		for (int i = 0; i < lune.size(); i++) 
 		{
-			//System.out.println(lune.get(i).getRel());
+			System.out.println(lune.get(i).getRel());
 			System.out.println(rc.getBody(lune.get(i).getRel(), false).getEnglishName());
 		}
+		System.out.println(rc.getBody("terre",true).getDensity());
 	}
 }
